adfa488b1ce65e2e0ad8a246ed9b56d7f47af610
Hiperagil1 <pinteaandrei12@gmail.com>
2023-04-07 02:21:32 +0300

    parse

--- a1/a1.c
+++ a1/a1.c
@@ -4,6 +4,9 @@
 #include <dirent.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
 
 int size_flag = 0;
 int perm_flag = 0;
@@ -95,6 +98,79 @@ void listRec(const char *path, long long int size, int suc_flag)
     closedir(dir);
 }
 
+typedef struct header {
+char magic[4];
+short header_size;
+int version;
+unsigned char no_of_sections;
+}Header;
+
+typedef struct section_header {
+char sect_name[13];
+int sect_type;
+int sect_offset;
+int sect_size;
+}Section_header;
+
+void parse(const char* file) {
+    FILE* fd = fopen(file, "rb");
+    if (fd == NULL) {
+        perror("could not open input file");
+        return;
+    }
+    Header h;
+   
+    fread(h.magic, sizeof(char), 4, fd);
+    fread(&h.header_size, sizeof(short), 1, fd);
+    fread(&h.version, sizeof(int), 1, fd);
+    fread(&h.no_of_sections, sizeof(unsigned char), 1, fd);
+    
+    Section_header s_h[h.no_of_sections];
+    memset(s_h, 0, sizeof(s_h)); // inițializare cu zero
+    for(int i =0; i<h.no_of_sections; i++){
+    	fread(&s_h[i].sect_name, sizeof(char), 12, fd);
+    	fread(&s_h[i].sect_type, sizeof(int), 1, fd);
+    	fread(&s_h[i].sect_offset, sizeof(int), 1, fd);
+    	fread(&s_h[i].sect_size, sizeof(int), 1, fd);    
+    }
+    fclose(fd);
+    
+    int ok = 1; //presupunem ca respecta formatul
+    if(!strcmp("WJUo", h.magic)){
+    	ok = 0;
+    	printf("ERROR\n");
+	printf("magic\n");
+    }
+    if(h.version < 37 || h.version > 133){
+    	ok = 0;
+    	printf("ERROR\n");
+	printf("version\n");
+    }
+    if(h.no_of_sections < 4 || h.no_of_sections > 19){
+    	ok = 0;
+    	printf("ERROR\n");
+	printf("sect_nr\n");
+    }
+    	
+    for(int i =0; i<h.no_of_sections; i++){
+	if(s_h[i].sect_type != 65 && s_h[i].sect_type != 95 && s_h[i].sect_type != 26 && s_h[i].sect_type != 96 && s_h[i].sect_type != 77 && s_h[i].sect_type != 37){
+		ok = 0;
+		printf("ERROR\n");
+		printf("sect_types\n");
+	}    
+    }
+    
+    if(ok == 1){
+    	printf("SUCCESS\n");
+    	printf("version=%d\n", h.version);
+    	printf("nr_sections=%d\n", h.no_of_sections);
+    	for(int i =0; i<h.no_of_sections; i++){
+    		
+    		printf("section%d: %.*s %d %d\n", i+1, (int)strlen(s_h[i].sect_name), s_h[i].sect_name, s_h[i].sect_type, s_h[i].sect_size);
+
+    	}
+    }
+}
 
 
 int main(int argc, char **argv){
@@ -128,10 +204,12 @@ int main(int argc, char **argv){
 			}
 			else{
 				listDir(argv[path]+5, size);
+			}
 		}
-		
-		
-		}
+    	}
+    	else if(strcmp(argv[1], "parse") == 0){
+    		parse(argv[2] + 5);
+    	
     	}	
     }
     return 0;
