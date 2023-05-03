37b0d50415c9edf09b69e0ac5837bbcf54315c71
Hiperagil1 <pinteaandrei12@gmail.com>
2023-05-04 00:14:54 +0300

    50.00

--- a2/a2.c
+++ a2/a2.c
@@ -4,6 +4,12 @@
 #include <sys/wait.h>
 #include <pthread.h>
 #include "a2_helper.h"
+#include <semaphore.h>
+#include <fcntl.h>
+
+
+sem_t sem3;
+sem_t sem5;
 
 typedef struct {
     int no_process;
@@ -12,8 +18,25 @@ typedef struct {
 
 void *threads(void *param) {
     TH_STRUCT *s = (TH_STRUCT *)param;
-    info(BEGIN, s->no_process, s->no_thread);
-    info(END, s->no_process, s->no_thread);
+    
+
+    if (s->no_thread == 3) {
+        // Firul de execuție 3 începe imediat
+        sem_wait(&sem5);
+        info(BEGIN, s->no_process, s->no_thread);
+        info(END, s->no_process, s->no_thread);
+        sem_post(&sem3);
+    }  else if (s->no_thread == 5) { 
+        // Firul de execuție 5 așteaptă semnalul semaforului 3 înainte de a începe execuția
+        info(BEGIN, s->no_process, s->no_thread);
+        sem_post(&sem5);
+        sem_wait(&sem3);
+        info(END, s->no_process, s->no_thread);
+    } else if (s->no_thread != 3 && s->no_thread != 5){
+        info(BEGIN, s->no_process, s->no_thread);
+        info(END, s->no_process, s->no_thread);
+    }
+
     return NULL;
 }
 
@@ -64,7 +87,18 @@ int main() {
 
                     TH_STRUCT params[5];
                     pthread_t tid[5];
-
+                    
+                    if(sem_init(&sem3, 0, 0) != 0){
+                        perror("Could not init the semaphore logSem.");
+                        return -1;
+                    }
+                    
+                    if(sem_init(&sem5, 0, 0) != 0){
+                        perror("Could not init the semaphore logSem.");
+                        return -1;
+                    }
+                    
+        
                     // Thread 1
                     params[0].no_process = 7;
                     params[0].no_thread = 1;
@@ -84,20 +118,21 @@ int main() {
                     params[2].no_process = 7;
                     params[2].no_thread = 3;
                     pthread_create(&tid[2], NULL, threads, &params[2]);
-                    pthread_join(tid[2], NULL);
-                    pthread_join(tid[4], NULL);
-                    
-                    
+                
                     // Thread 4
                     params[3].no_process = 7;
                     params[3].no_thread = 4;
                     pthread_create(&tid[3], NULL, threads, &params[3]);
+                    
+                    
                     pthread_join(tid[3], NULL);
-
+                    pthread_join(tid[2], NULL);     
                     pthread_join(tid[0], NULL);
                     pthread_join(tid[1], NULL);
+                    pthread_join(tid[4], NULL);
                         
-
+                    sem_destroy(&sem3);
+                    sem_destroy(&sem5);
                     info(END, 7, 0);
                 } else {
                     wait(NULL);
@@ -109,4 +144,3 @@ int main() {
 
     return 0;
 }
-
